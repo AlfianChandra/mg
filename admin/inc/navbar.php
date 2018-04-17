48534fd92fd346e0ef59c3773978c252873cf768
Alfian Chandra <alfian.github404@gmail.com>
2018-04-17 10:39:41 +0700

    Baru...

--- admin/inc/navbar.php
+++ admin/inc/navbar.php
@@ -8,18 +8,24 @@
         <div class="sidebar-wrapper">
           <ul class="nav">
             <li>
+                <a href="index.php">
+                    <i class="material-icons">dashboard</i>
+                    <p>Beranda Admin</p>
+                </a>
+            </li>            
+            <li>
               <a href="add-student.php">
                 <i class="material-icons">settings</i>
                 <p>Manage Mahasiswa</p>
               </a>
             </li>
 
-              <li>
-                  <a href="add-dosen.php">
-                      <i class="material-icons">settings</i>
-                      <p>Manage Dosen</p>
-                  </a>
-              </li>
+            <li>
+                <a href="add-dosen.php">
+                    <i class="material-icons">settings</i>
+                    <p>Manage Dosen</p>
+                </a>
+            </li>
           </ul>
         </div>
       </div>
