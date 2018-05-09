65f443ab0ac0b0e066f6caab8f2a2ebb0329bfd2
khatamihusaini <38415616+khatamihusaini@users.noreply.github.com>
2018-05-09 17:27:28 +0700

    eh db jangan lupa di import ya ,

--- auth/index.php
+++ auth/index.php
@@ -160,70 +160,7 @@
       }
     </style>
   </head>
-<<<<<<< HEAD
-  <body>
-    <div class="limiter">
-      <div class="container-login100" style='height:100px;'>
-        <div class="wrap-login100">
 
-          <form class="login100-form" method="POST" action="../action/login.php">
-            <span class="login100-form-title p-b-34">
-            LOGIN
-            </span>
-            <div class="wrap-input100 rs1-wrap-input100 validate-input m-b-20" data-validate="Type user name">
-              <input id="first-name" class="input100" type="text" name="username" placeholder="Nama Pengguna">
-              <span class="focus-input100"></span>
-            </div>
-            <div class="wrap-input100 rs2-wrap-input100 validate-input m-b-20" data-validate="Type password">
-              <input class="input100" type="password" name="pass" placeholder="Kata Sandi">
-              <span class="focus-input100"></span>
-            </div>
-            <div class="container-login100-form-btn">
-              <input type="submit" class="login100-form-btn" value="Sign in">
-            </div>
-          </form>
-          
-          <div class="login100-more">
-            <img src='https://siakadlama.unja.ac.id/foto/F1E115015.JPG' style='height:100%;width:16.666%;object-fit: cover;float:left;'>
-            <img src='https://siakadlama.unja.ac.id/foto/F1E115010.JPG' style='height:100%;width:16.666%;object-fit: cover;float:left;'>
-            <img src='https://siakadlama.unja.ac.id/foto/F1E115004.JPG' style='height:100%;width:16.666%;object-fit: cover;float:left;'>
-            <img src='https://siakadlama.unja.ac.id/foto/F1E115034.JPG' style='height:100%;width:16.666%;object-fit: cover;float:left;'>
-            <img src='https://siakadlama.unja.ac.id/foto/F1E115029.JPG' style='height:100%;width:16.666%;object-fit: cover;float:left;'>
-            <img src='https://siakadlama.unja.ac.id/foto/F1E115021.JPG' style='height:100%;width:16.666%;object-fit: cover;float:left;'>
-            <style>
-              .overlays
-              {
-                position:absolute;
-                top:0;
-                bottom:0;
-                width:100%;
-                height:100%;
-                background-color:rgba(0,0,0,0.7);
-              }
-            </style>
-            <div class='overlays'>
-              <h2 style='text-align:center;color:#fff;margin-top:20px;'><span style='color:purple;font-style: italic'>INTERN</span>APP<br><br>SISTEM INFORMASI MAGANG<br>
-              KELOMPOK 7</h2>
-              <div class='write' style='margin-top:25px;padding:20px;'>
-                <h5 style='text-align: left;color:#fff;font-size:13px;'>Lead Project Manager: &nbsp;&nbsp;&nbsp;&nbsp; <b>Yulia Oktaviani</b></h5>
-                <h5 style='text-align: left;color:#fff;font-size:13px;'>System Analyst Specialists: &nbsp;&nbsp;&nbsp;&nbsp; <b>Adam Rahmat Fitrianto</b>, <b>Al Ikhzan</b></h5>
-                <h5 style='text-align: left;color:#fff;font-size:13px;'>Lead DataBase Expert: &nbsp;&nbsp;&nbsp;&nbsp; <b>Muhammad Khatami Husaini</b></h5>
-                <h5 style='text-align: left;color:#fff;font-size:13px;'>Lead Programmer: &nbsp;&nbsp;&nbsp;&nbsp; <b>Rahmad Arif Irwansyah</b></h5>
-                <h5 style='text-align: left;color:#fff;font-size:13px;'>Lead Interface Designer: &nbsp;&nbsp;&nbsp;&nbsp; <b>Alfian Chandra</b></h5>
-                <address style='color:#fff;margin-top: 15px;font-style: italic;font-size:12px;'>&COPY; INTERNAPP. Made with &hearts; and special touches by 7th Team &centerdot; Information System &centerdot; University of Jambi</address>
-              </div>
-            </div>
-          </div>
-        </div>
-=======
-  <body style="background-color:#ccc;">
-    
-    <div class="alr-wrapper">
-      <div class="container">
-        <h4 class="alr-content" style="font-size:18px;margin:0;color:#fff;text-transform: uppercase;font-weight: bold;"></h4>
->>>>>>> cfc0127fb2cfe60ecb7636939e2065cb20dcac4f
-      </div>
-    </div>
     
     <div class="lg-wrapper">
       <div class="lg-heading">UNJA Internship Application &nbsp;&nbsp; &centerdot; &nbsp;&nbsp; System Gateway</div>
